9d9c0e36c20e74cf6447fd3bccc8d7968833bd3f
amirachaudhry <ac2106@hw.ac.uk>
2024-05-24 16:42:17 +0100

    Added text bar and a delete function

--- src/App.js
+++ src/App.js
@@ -10,24 +10,42 @@ const App = () => {
   const handleTasks = () => {
     // does exist
     if (newTodo.trim() != "") {
-      setTodos([...todos, {text: newTodo.trim(), check: false}]);
+      setTodos([...todos, {text: newTodo.trim(), checked: false}]);
       setNewTodo("");
 
     }
   };
 
   const handleDelete = (index) => {
-    
-  };
+    const newTodos = [...todos];
+    newTodos.splice(index,1);
+    setTodos(newTodos);
+  }
 
   const handleToggle = (index) => {
+
     
   };
   return (
     <div>
       <TodoList/>
+      <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)}/>
+      <button onClick={(handleTasks)}>Add</button>
+
+      <ul>
+        {todos.map((todos,index) => (
+          <li key={index}>
+            <input type="checkbox" checked={todos.checked} onChange={ () => handleToggle(index)}
+          />
+          <span>
+            {todos.text}
+          </span>
+          <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
